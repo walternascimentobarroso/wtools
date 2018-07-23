f44dbbf9dd5d87d15919044c72e9bedef45010ff
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2018-07-22 20:11:03 -0400

    [ADJUST] New feutures for cpf page
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/js/cpf.js
+++ src/js/cpf.js
@@ -36,4 +36,73 @@ document.addEventListener("DOMContentLoaded", function () {
             return false;
         return true;
     }
+
+    function valida_cpf(cpf) {
+        var numeros, digitos, soma, i, resultado, digitos_iguais;
+        digitos_iguais = 1;
+        if (cpf.length < 11)
+            return false;
+        for (i = 0; i < cpf.length - 1; i++)
+            if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+                digitos_iguais = 0;
+                break;
+            }
+        if (!digitos_iguais) {
+            numeros = cpf.substring(0, 9);
+            digitos = cpf.substring(9);
+            soma = 0;
+            for (i = 10; i > 1; i--)
+                soma += numeros.charAt(10 - i) * i;
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(0))
+                return false;
+            numeros = cpf.substring(0, 10);
+            soma = 0;
+            for (i = 11; i > 1; i--)
+                soma += numeros.charAt(11 - i) * i;
+            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+            if (resultado != digitos.charAt(1))
+                return false;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    function randomiza(n) {
+        var ranNum = Math.round(Math.random() * n);
+        return ranNum;
+    }
+
+    function mod(dividendo, divisor) {
+        return Math.round(dividendo - (Math.floor(dividendo / divisor) * divisor));
+    }
+
+    function gerarCPF() {
+        let comPontos = true; // TRUE para ativar e FALSE para desativar a pontuação.
+        let cpf;
+        var n = 9;
+        var n1 = randomiza(n);
+        var n2 = randomiza(n);
+        var n3 = randomiza(n);
+        var n4 = randomiza(n);
+        var n5 = randomiza(n);
+        var n6 = randomiza(n);
+        var n7 = randomiza(n);
+        var n8 = randomiza(n);
+        var n9 = randomiza(n);
+        var d1 = n9 * 2 + n8 * 3 + n7 * 4 + n6 * 5 + n5 * 6 + n4 * 7 + n3 * 8 + n2 * 9 + n1 * 10;
+        d1 = 11 - (mod(d1, 11));
+        if (d1 >= 10) d1 = 0;
+        var d2 = d1 * 2 + n9 * 3 + n8 * 4 + n7 * 5 + n6 * 6 + n5 * 7 + n4 * 8 + n3 * 9 + n2 * 10 + n1 * 11;
+        d2 = 11 - (mod(d2, 11));
+        if (d2 >= 10) d2 = 0;
+        let retorno = '';
+        if (comPontos) cpf = '' + n1 + n2 + n3 + '.' + n4 + n5 + n6 + '.' + n7 + n8 + n9 + '-' + d1 + d2;
+        else cpf = '' + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + d1 + d2;
+
+        alert(cpf);
+    }
+
+    gerarCPF();
 });
