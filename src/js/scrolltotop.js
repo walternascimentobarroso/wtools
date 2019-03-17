30c2a69444730192f685dcf11f598433a0e5faec
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2019-03-17 11:51:13 -0400

    [ADD] New version scrolltotop

--- src/js/scrolltotop.js
+++ src/js/scrolltotop.js
@@ -1,39 +1,70 @@
 $(function () {
-  "use strict";
-  let content = `<a href="#" id="scrollToTop"> <i class="fas fa-arrow-up centerarrow"></i></a>`;
-  $('body').append(content);
+    "use strict";
+    let content = `<a href="#" id="scrollToTop"> <i class="fas fa-arrow-up centerarrow"></i></a>`;
+    $('body').append(content);
 
-  $(document).ready(function () {
-    document.getElementById('scrollToTop').onclick = function () {
-        scrollTo(document.documentElement, 0, 1250);   
-     }
-         
-     function scrollTo(element, to, duration) {
-         var start = element.scrollTop,
-             change = to - start,
-             currentTime = 0,
-             increment = 20;
-             
-         var animateScroll = function(){        
-             currentTime += increment;
-             var val = Math.easeInOutQuad(currentTime, start, change, duration);
-             element.scrollTop = val;
-             if(currentTime < duration) {
-                 setTimeout(animateScroll, increment);
-             }
-         };
-         animateScroll();
-     }
-     
-     //t = current time
-     //b = start value
-     //c = change in value
-     //d = duration
-     Math.easeInOutQuad = function (t, b, c, d) {
-       t /= d/2;
-         if (t < 1) return c/2*t*t + b;
-         t--;
-         return -c/2 * (t*(t-2) - 1) + b;
-     };
-  });
+    $(document).ready(function () {
+        document.getElementById('scrollToTop').onclick = function () {
+            scrollTo(0, 1250);
+            // scrollTo2(document.documentElement, 0, 1250);
+        }
+
+
+        const scrollTo = function (to, duration) {
+            const element = document.scrollingElement || document.documentElement,
+                start = element.scrollTop,
+                change = to - start,
+                startDate = +new Date(),
+                // t = current time
+                // b = start value
+                // c = change in value
+                // d = duration
+                easeInOutQuad = function (t, b, c, d) {
+                    t /= d / 2;
+                    if (t < 1) return c / 2 * t * t + b;
+                    t--;
+                    return -c / 2 * (t * (t - 2) - 1) + b;
+                },
+                animateScroll = function () {
+                    const currentDate = +new Date();
+                    const currentTime = currentDate - startDate;
+                    element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+                    if (currentTime < duration) {
+                        requestAnimationFrame(animateScroll);
+                    }
+                    else {
+                        element.scrollTop = to;
+                    }
+                };
+            animateScroll();
+        };
+
+        function scrollTo(element, to, duration) {
+            var start = element.scrollTop,
+                change = to - start,
+                currentTime = 0,
+                increment = 20;
+
+            var animateScroll = function () {
+                currentTime += increment;
+                var val = Math.easeInOutQuad(currentTime, start, change, duration);
+                element.scrollTop = val;
+                if (currentTime < duration) {
+                    setTimeout(animateScroll, increment);
+                }
+            };
+            animateScroll();
+        }
+
+        //t = current time
+        //b = start value
+        //c = change in value
+        //d = duration
+        Math.easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    });
 });
